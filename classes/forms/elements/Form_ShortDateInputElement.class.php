18c5d6160b472fb9c0dc803e08c8327818e24034
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-04-15 11:17:48 -0700

    Replace depriciated split with explode in ShortDateElement.

--- classes/forms/elements/Form_ShortDateInputElement.class.php
+++ classes/forms/elements/Form_ShortDateInputElement.class.php
@@ -19,7 +19,7 @@ class Form_ShortDateInputElement extends Form_InputElement{
       //create a date using the first day of the month
       $input->{$this->name} = trim($input->$year) . "-{$input->$month}-1";
     } else if(!is_null($input->{$this->name})){
-      $arr = split(' ', $input->{$this->name});
+      $arr = explode(' ', $input->{$this->name});
       $input->{$this->name} = "{$arr[0]} 1 {$arr[1]}";
     }
     return parent::validate($input);
