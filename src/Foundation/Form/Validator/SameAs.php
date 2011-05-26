b56c7bed8ed256d04fd478cb9e9ead9b0d6036b6
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-05-25 20:14:03 -0700

    fix SameAs validator class name and validate signature

--- src/Foundation/Form/Validator/SameAs.php
+++ src/Foundation/Form/Validator/SameAs.php
@@ -3,8 +3,8 @@ namespace Foundation\Form\Validator;
 /**
  * Check to see if the value is the same as another value
  */
-class SaneAs extends AbstractValidator{
-  public function validate(FormInput $input){
+class SameAs extends AbstractValidator{
+  public function validate(\Foundation\Form\Input $input){
     if($input->get($this->e->getName()) != $input->get($this->ruleSet)){
       $this->addError('Does not match ' . $this->e->getField()->getForm()->getElementByname($this->ruleSet)->getLabel());
       return false;
