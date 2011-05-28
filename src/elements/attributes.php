1f330bb2d1a15ee63c015fdee784a5ec34eef4b5
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-05-28 12:29:53 -0700

    check attribute accessiblity in attributes element

--- src/elements/attributes.php
+++ src/elements/attributes.php
@@ -2,8 +2,9 @@
 /**
  * HTML attributes element
  */
-foreach($object->getAttributes() as $memberName => $htmlName){
+  foreach($object->getAttributes() as $memberName => $htmlName){
   $method = 'get' . ucfirst($memberName);
+  if(!method_exists($object, $method)) throw new \Foundation\Exception("Unable to access {$memberName} using {$method} on " . get_class($object));
   $value = $object->$method();
   if(!is_null($value)) print ' ' . $htmlName . '="' . htmlentities($value) . '"';
 }
