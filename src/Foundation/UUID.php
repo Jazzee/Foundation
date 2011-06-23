ae4fbfe7dd51053e41a46f03514e5dac62321ac4
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-06-22 23:12:29 -0700

    use PECL UUID since it actually works.  If it is not present then try our own generator

--- src/Foundation/UUID.php
+++ src/Foundation/UUID.php
@@ -3,69 +3,56 @@ namespace Foundation;
 /**
  * Generate UUIDs 
  * 
- * Required php5-uuid funtctions
+ * Attempts to use the PECL UUID library
  * Stolen from comments in PHP manual: http://www.php.net/manual/en/function.uniqid.php
  */
 class UUID {
-  
-  public static function v1() {
-    if (!function_exists('uuid_create'))
-      throw new Exception('uuid_create function required.  Install php5-uuid package.');
-
-    uuid_create(&$context);
-    uuid_make($context, UUID_MAKE_V1);
-    uuid_export($context, UUID_FMT_STR, &$uuid);
-    return trim($uuid);
-  }
-
-  /**
-   * Generates version 3 UUID: MD5 hash of URL
-   */
-  public static function v3($i_url) {
-    if (!function_exists('uuid_create'))
-      throw new Exception('uuid_create function required.  Install php5-uuid package.');
-
-    if (!strlen($i_url))
-      $i_url = self::v1();
-
-    uuid_create(&$context);
-    uuid_create(&$namespace);
-
-    uuid_make($context, UUID_MAKE_V3, $namespace, $i_url);
-    uuid_export($context, UUID_FMT_STR, &$uuid);
-    return trim($uuid);
-  }
-
   /**
    * Generates version 4 UUID: random
    */
   public static function v4() {
-    if (!function_exists('uuid_create'))
-      throw new Exception('uuid_create function required.  Install php5-uuid package.');
-
-    uuid_create(&$context);
-
-    uuid_make($context, UUID_MAKE_V4);
-    uuid_export($context, UUID_FMT_STR, &$uuid);
-    return trim($uuid);
-  }
-
-  /**
-   * Generates version 5 UUID: SHA-1 hash of URL
-   */
-  public static function v5($i_url) {
-    if (!function_exists('uuid_create'))
-      throw new Exception('uuid_create function required.  Install php5-uuid package.');
-
-    if (!strlen($i_url))
-      $i_url = self::v1();
-
-    uuid_create(&$context);
-    uuid_create(&$namespace);
-
-    uuid_make($context, UUID_MAKE_V5, $namespace, $i_url);
-    uuid_export($context, UUID_FMT_STR, &$uuid);
-    return trim($uuid);
+    //try and use the PECL UUID library
+    if (!function_exists('uuid_create')) return uuid_create(UUID_TYPE_RANDOM);
+    
+    $pr_bits = null;
+    $fp = @fopen('/dev/urandom','rb');
+    if ($fp !== false) {
+      $pr_bits .= @fread($fp, 16);
+      @fclose($fp);
+    } else {
+      // If /dev/urandom isn't available (eg: in non-unix systems), use mt_rand().
+      $pr_bits = "";
+      for($cnt=0; $cnt < 16; $cnt++){
+        $pr_bits .= chr(mt_rand(0, 255));
+      }
+    }
+   
+    $time_low = bin2hex(substr($pr_bits,0, 4));
+    $time_mid = bin2hex(substr($pr_bits,4, 2));
+    $time_hi_and_version = bin2hex(substr($pr_bits,6, 2));
+    $clock_seq_hi_and_reserved = bin2hex(substr($pr_bits,8, 2));
+    $node = bin2hex(substr($pr_bits,10, 6));
+   
+    /**
+     * Set the four most significant bits (bits 12 through 15) of the
+     * time_hi_and_version field to the 4-bit version number from
+     * Section 4.1.3.
+     * @see http://tools.ietf.org/html/rfc4122#section-4.1.3
+     */
+    $time_hi_and_version = hexdec($time_hi_and_version);
+    $time_hi_and_version = $time_hi_and_version >> 4;
+    $time_hi_and_version = $time_hi_and_version | 0x4000;
+   
+    /**
+     * Set the two most significant bits (bits 6 and 7) of the
+     * clock_seq_hi_and_reserved to zero and one, respectively.
+     */
+    $clock_seq_hi_and_reserved = hexdec($clock_seq_hi_and_reserved);
+    $clock_seq_hi_and_reserved = $clock_seq_hi_and_reserved >> 2;
+    $clock_seq_hi_and_reserved = $clock_seq_hi_and_reserved | 0x8000;
+   
+    return sprintf('%08s-%04s-%04x-%04x-%012s',
+        $time_low, $time_mid, $time_hi_and_version, $clock_seq_hi_and_reserved, $node);
   }
 }
 ?>
