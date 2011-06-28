0acc39f54118bd3622850725dcf790b4b43131a0
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-06-28 10:30:44 -0700

    ability to add links to container not in a menu

--- src/Foundation/Navigation/Container.php
+++ src/Foundation/Navigation/Container.php
@@ -10,12 +10,19 @@ class Container extends \Foundation\HTMLElement{
   protected $menus;
   
   /**
+   * The Links
+   * @var array of \Foundation\Navitation\Link
+   */
+  protected $links;
+  
+  /**
    * Construct
    * 
    */
   public function __construct(){
     parent::__construct();
     $this->menus = array();
+    $this->links = array();
   }
   
   /**
@@ -32,6 +39,30 @@ class Container extends \Foundation\HTMLElement{
    */
   public function getMenus(){
     return $this->menus;
+  }
+  
+  /**
+   * Add a link
+   * @param \Foundation\Navigation\Link $link
+   */
+  public function addLink(\Foundation\Navigation\Link $link){
+    $this->links[] = $link;
+  }
+  
+  /**
+   * Get the links
+   * return array
+   */
+  public function getLinks(){
+    return $this->links;
+  }
+  
+  /**
+   * Does the container have links
+   * @return bool true if there are any links false if not
+   */
+  public function hasLink(){
+    return (bool)count($this->links);
   }
 }
 ?>
--- src/elements/navigation.php
+++ src/elements/navigation.php
@@ -4,13 +4,24 @@
  */
 ?>
 <div<?php $this->renderElement('attributes', array('object'=>$container)); ?>>
-  <ol><?php
-  foreach($container->getMenus() as $menu){
-    if($menu->hasLink()){
-      echo '<li>';
-      $this->renderElement('menu', array('menu'=>$menu));
-      echo '</li>';
+  <ol>
+    <?php
+    if($container->hasLink()){
+      foreach($container->getLinks() as $link){
+        echo '<li>';
+          $this->renderElement('link', array('link'=>$link));
+        echo '</li>';
+      }
     }
-  }
-  ?></ol>
+    ?>
+    <?php
+    foreach($container->getMenus() as $menu){
+      if($menu->hasLink()){
+        echo '<li>';
+        $this->renderElement('menu', array('menu'=>$menu));
+        echo '</li>';
+      }
+    }
+    ?>
+  </ol>
 </div>
