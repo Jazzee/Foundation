2f60d3990cbc5b5a3fae13e6a41aedfa1f41821a
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-10-19 12:46:41 -0700

    fix default value in radio list

--- src/elements/RadioList.control.php
+++ src/elements/RadioList.control.php
@@ -6,9 +6,13 @@
  <ol>
 <?php foreach($element->getItems() as $id => $item){
   echo "<li><input type='radio'";
-  if($element->getValue() == $item->getValue()){
-    print ' checked="checked"';
-  }
+  if($element->getValue()){
+    if($element->getValue() == $item->getValue()){
+      print ' checked="checked"';
+    }
+  } else if($element->getDefaultValue() == $item->getValue()){
+      print ' checked="checked"';
+    }
   echo ' value="' . $item->getValue() . '" name="' . $element->getName() . '" id="' . $element->getName() . "_{$id}\" />" .
       '<label for="' . $element->getName() . "_{$id}\">" . $item->getLabel() . '</label>' .
       '</li>';
