addf71e24d42fa473df2e8c33234768523611c88
Jonathan Johnson <jon.johnson@ucsf.edu>
2011-11-15 22:57:01 -0800

    add Virusscan validator which uses the php-clamav library to scan uploaded files for a virus

---
+++ src/Foundation/Form/Validator/Virusscan.php
@@ -0,0 +1,38 @@
+<?php
+namespace Foundation\Form\Validator;
+/**
+ * Scan the input for viruses
+ * Uses the php-clamav http://php-clamav.sourceforge.net/
+ * Which depends on clamav
+ * @author Jon Johnson <jon.johnson@ucsf.edu>
+ * @license http://jazzee.org/license.txt
+ * @package foundation
+ * @subpackage forms
+ */
+class Virusscan extends AbstractValidator{
+  
+  /**
+   * Do our own constructor so we can check for the php-clamav library
+   * @param \Foundation\Form\Element $e the element we are validating
+   * @param integer $sizeInBytes
+   */
+  public function  __construct(\Foundation\Form\Element $element){
+    if(!extension_loaded('clamav') or !function_exists('cl_scanfile')){
+      throw new \Foundation\Exception("Virusscan validator requires the php-clamav extension.");
+    }
+    parent::__construct($element);
+  }
+  
+  public function validate(\Foundation\Form\Input $input){
+    $fileArr = $input->get($this->e->getName());
+    $virusname = '';
+    $retcode = cl_scanfile($fileArr['tmp_name'], $virusname);
+    if($retcode == CL_VIRUS) {
+      unlink($fileArr['tmp_name']);
+      $this->addError('Virus Detection Error: ' . cl_pretcode($retcode) . " {$virusname}.");
+      return false;
+    }
+    return true;
+  }
+}
+?>
