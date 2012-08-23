9798d82af160109409507b8d2d9603f8b68e12f6
Jonathan Johnson <jon.johnson@ucsf.edu>
2012-08-23 15:13:35 -0700

    File not found user error message for Virtual file exceptions.

--- src/Foundation/Virtual/Exception.php
+++ src/Foundation/Virtual/Exception.php
@@ -2,19 +2,19 @@
 namespace Foundation\Virtual;
 /**
  * Exceptions for Virtual Files
- * 
+ *
  * Declare an exception class for Foundation\Virtual
  * Allows us to easily send error codes
- * 
+ *
  * @package Foundation\virtual
 */
 class Exception extends \Foundation\Exception
 {
   protected $_httpErrorCode;
-  /**  
+  /**
    * Override the default Exception class constructor
    * Take the file name and the Error code and create a new Exception
-   * 
+   *
    * @param string $fileName
    * @param int $httpErrorCode HTTP error code
    */
@@ -22,12 +22,12 @@ class Exception extends \Foundation\Exception
   {
     $this->_httpErrorCode = $httpErrorCode;
     $message = "Attempting to access {$fileName} resulted in {$httpErrorCode}";
-    parent::__construct($message);
+    parent::__construct($message, E_USER_NOTICE, 'File not found.');
   }
 
-  /**  
+  /**
    * Returns httd error code
-   * 
+   *
    * @return integer
    */
   public function getHttpErrorCode()
