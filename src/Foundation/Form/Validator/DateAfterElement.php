d1bcc84f7c761f23552e3ad6b66205b2e4182158
Jonathan Johnson <jon.johnson@ucsf.edu>
2013-05-10 08:52:06 -0700

    Add DateAfterElement validator.

---
+++ src/Foundation/Form/Validator/DateAfterElement.php
@@ -0,0 +1,22 @@
+<?php
+namespace Foundation\Form\Validator;
+/**
+ * Check to see if the date entered is after the date in another element
+ * 
+ * @package Foundation\form\validator
+ * @author  Jon Johnson <jon.johnson@ucsf.edu>
+ * @license BSD http://jazzee.org/license.html
+ */
+class DateAfterElement extends AbstractValidator
+{
+  public function validate(\Foundation\Form\Input $input)
+  {
+    if (strtotime($input->get($this->e->getName())) <= strtotime($input->get($this->ruleSet))) {
+      $this->addError('Must be after date in ' . $this->e->getField()->getForm()->getElementByName($this->ruleSet)->getLabel());
+
+      return false;
+    }
+
+    return true;
+  }
+}
