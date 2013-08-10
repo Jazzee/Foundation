1fb86a31b8464471b818eb4e57898ab630b504fc
Jonathan Johnson <jon.johnson@ucsf.edu>
2013-08-10 11:00:59 -0700

    Get unit tests working with composer.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,22 +3,4 @@
  * Setup Tests
  * 
  */
-
-error_reporting(E_ALL | E_STRICT);
-
-require_once 'Doctrine/Common/ClassLoader.php';
-
-$classLoader = new \Doctrine\Common\ClassLoader('', __DIR__);
-$classLoader->register();
-
-$classLoader = new \Doctrine\Common\ClassLoader('Foundation',  __DIR__ . '/../src');
-$classLoader->register();
-
-$classLoader = new \Doctrine\Common\ClassLoader('Doctrine');
-$classLoader->register();
-
-set_include_path(
-    __DIR__  . PATH_SEPARATOR . get_include_path()
-);
-
 require_once 'TestCase.php';
