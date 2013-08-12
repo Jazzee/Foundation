e07fa0cd0d8543a10b30aa21d4d336366c80b2d7
Jonathan Johnson <jon.johnson@ucsf.edu>
2013-08-12 12:12:35 -0700

    Split the Safe filter into a version for HTML enabled input and a plain text one. The plain text input filter just htmlentities everything and the HTML one uses HTMLPurifier to fix the input.

---
+++ tests/Form/Filter/FormFilterSafeHTMLTest.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once __DIR__ . '/../../bootstrap.php';
+
+class FormFilterSafeHTMLTest extends TestCase
+{
+
+    public function testFilter()
+    {
+        $element = $this->getMockBuilder('\Foundation\Form\Element\ListElement')
+                        ->disableOriginalConstructor()->getMock();
+        $object = new \Foundation\Form\Filter\SafeHTML($element);
+        $this->assertEquals(
+            "&lt;script&gt;alert('XSS')&lt;/script&gt;»&gt;",
+            $object->filterValue("<SCRIPT>alert('XSS')</SCRIPT »>")
+        );
+        $test = "';alert(String.fromCharCode(88,83,83))//'" .
+                ';alert(String.fromCharCode(88,83,83))//"' .
+                'alert(String.fromCharCode(88,83,83))//";' .
+                'alert(String.fromCharCode(88,83,83))//--></SCRIPT>">' .
+                "><SCRIPT>alert(String.fromCharCode(88,83,83))</SCRIPT>";
+        $result = "';alert(String.fromCharCode(88,83,83))//'" .
+                ';alert(String.fromCharCode(88,83,83))//"alert(String.fromCharCode(88,83,83))//";' .
+                'alert(String.fromCharCode(88,83,83))//--&gt;"&gt;&gt;&lt;script&gt;' .
+                'alert(String.fromCharCode(88,83,83))&lt;/script&gt;';
+        $this->assertEquals($result, $object->filterValue($test));
+    }
+}
